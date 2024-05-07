4759a9b0cb33aee1dd382f67742aab4c4ed8c852
Carl Hostetler <carl.hostetler@gmail.com>
2024-05-06 18:52:29 -0700

    IDK, seems good to me

--- seed/seed.js
+++ seed/seed.js
@@ -1,9 +1,8 @@
 import knex from "../src/lib/knex.js";
 
 const run = async () => {
-    const tableExists = await knex.schema.hasTable('plants')
     
-    if (!tableExists) {
+    if (!(await knex.schema.hasTable('plants'))) {
         await knex.schema.createTable("plants", function (table) {
           table.increments("id");
           table.string("name");
@@ -15,6 +14,18 @@ const run = async () => {
         
         await knex('plants').insert({name: 'tomatoes', modified: new Date(Date.now()).toISOString(), start: 8, harvest: 70, plant: (new Date('May 01, 2024 00:00:00 GMT-7:00')).toISOString()})
     }
+
+    if (!(await knex.schema.hasTable('events'))) {
+      await knex.schema.createTable('events', function (table) {
+        table.increments("id");
+        table.string("message");
+        table.integer("day");
+        table.integer("month");
+      })
+
+    }
+
+
 
     knex.destroy();
 }
--- src/lib/updateSchedule.js
+++ src/lib/updateSchedule.js
@@ -1,19 +1,34 @@
-import knex from "./knex.js"
+import knex from "./knex.js";
 
 export default async function handler(Astro) {
-    let result = []
-    if (Astro.request.method === "POST") {
+  let result = [];
+  
+  if (Astro.request.method === "POST") {
+    console.log("we be posting")
     try {
-      const data = await Astro.request.formData()
-      if (data.get('tomatoes')) {
-        result = await knex.select().table('plants')
+      const data = await Astro.request.formData();
+      if (data.get("tomatoes")) {
+        await knex("events").insert({
+          message: "Start tomatoes indoors",
+          day: 3,
+          month: 2,
+        });
+        await knex("events").insert({
+          message: "Transplant tomatoes outdoors",
+          day: 5,
+          month: 4,
+        });
+        await knex("events").insert({
+          message: "Start harvesting tomatoes",
+          day: 7,
+          month: 6,
+        });
       }
-      
     } catch (error) {
       if (error instanceof Error) {
-          console.error(error.message)
+        console.error(error.message);
       }
     }
   }
-  return result
+  return await knex.select().table("events");
 }
